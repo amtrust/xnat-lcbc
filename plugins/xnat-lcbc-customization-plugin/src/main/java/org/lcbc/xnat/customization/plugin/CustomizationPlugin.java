80eb6f477138f59cff2f23f4f0720c682419b0d5
Inge Kasbohm Amlien <ingeam@1x-193-157-182-149.uio.no>
2017-04-28 10:37:01 +0200

    added plugins

---
+++ plugins/xnat-lcbc-customization-plugin/src/main/java/org/lcbc/xnat/customization/plugin/CustomizationPlugin.java
@@ -0,0 +1,9 @@
+package org.lcbc.xnat.customization.plugin;
+
+import org.nrg.framework.annotations.XnatPlugin;
+
+@XnatPlugin(value = "CustomizationPlugin", name = "LCBC XNAT 1.7 Customization Plugin", 
+            description = "Plugin for custromizing various screens and features of LCBCs TSD/Xnat installation")
+
+public class CustomizationPlugin {
+}
